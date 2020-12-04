b7448caac12652925dc2ab06ef63990eb7e997d8
ArtIPok <apokora@itsap.pl>
2020-12-04 13:41:12 +0100

    Modified script.js

--- js/script.js
+++ js/script.js
@@ -2,4 +2,5 @@
 let computerMove = 'kamień';
 let playerMove = 'papier';
 
-printMessage('Zagrałem ' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!')
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!')
+printMessage('Zagrałem' + playerMove + '! Jeśli Twój ruch to nożyczki, to wygrywasz')
